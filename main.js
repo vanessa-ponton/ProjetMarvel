afc89c4a32f214c47a1f98c7cd12651f0ee0ad0d
Vanessa Ponton <vanessa.ponton@gmail.com>
2023-11-10 16:07:20 +0100

    YoussefaddJS

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 // Afficher les classes les unes apres les autres
-document.getElementsByClassName('Welcome-screen');
-document.getElementsByClassName('Question1-screen');
-document.getElementsByClassName('Question2-screen');
-document.getElementsByClassName('Question3-screen');
-document.getElementsByClassName('End-screen');
 
 // Afficher la classe "welcome-screen" et quand l'utilisateur clique sur le bouton "c'est parti", lancer la 1ere question
 // Afficher la classe "question1-screen" et quand l'utilisateur clique sur le bouton : 2 reponses possibles soit vraie soit faux
