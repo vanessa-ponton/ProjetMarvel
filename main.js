448a3b7bd23cafd6f8a64428875663e454ebc62c
Vanessa Ponton <vanessa.ponton@gmail.com>
2023-11-10 15:22:18 +0100

    addJS

---
+++ main.js
@@ -0,0 +1,11 @@
+// Afficher les classes les unes apres les autres
+document.getElementsByClassName('Welcome-screen');
+document.getElementsByClassName('Question1-screen');
+document.getElementsByClassName('Question2-screen');
+document.getElementsByClassName('Question3-screen');
+document.getElementsByClassName('End-screen');
+
+// Afficher la classe "welcome-screen" et quand l'utilisateur clique sur le bouton "c'est parti", lancer la 1ere question
+// Afficher la classe "question1-screen" et quand l'utilisateur clique sur le bouton : 2 reponses possibles soit vraie soit faux
+// Récolter les résultats et passer à la classe suivante X 2
+// Afficher la classe "end-screen" pour afficher le score et la phrase selon le score obtenu
