5cf9c65423ad0a852f6837475df1bc6ffd3250d0
Nadir KHOUANI <khouani.nadir7@gmail.com>
2023-11-10 16:04:22 +0100

    Co-authored-by: YoussefSlim <YoussefSlim@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
 // Afficher les classes les unes apres les autres
-document.getElementsByClassName('Welcome-screen');
-document.getElementsByClassName('Question1-screen');
-document.getElementsByClassName('Question2-screen');
-document.getElementsByClassName('Question3-screen');
-document.getElementsByClassName('End-screen');
+// document.getElementsByClassName('Welcome-screen');
+// document.getElementsByClassName('Question1-screen');
+// document.getElementsByClassName('Question2-screen');
+// document.getElementsByClassName('Question3-screen');
+// document.getElementsByClassName('End-screen');
 
 // Afficher la classe "welcome-screen" et quand l'utilisateur clique sur le bouton "c'est parti", lancer la 1ere question
 // Afficher la classe "question1-screen" et quand l'utilisateur clique sur le bouton : 2 reponses possibles soit vraie soit faux
 // Récolter les résultats et passer à la classe suivante X 2
 // Afficher la classe "end-screen" pour afficher le score et la phrase selon le score obtenu
+// stocker le score 
+let score = 0;
+// on doit clic sur c'est parti
+const start = document.querySelector("#start")
+// cibler les questions
+const question1 = document.querySelector("#q1")
+const question2 = document.querySelector("#q2")
+// ecouter l'event click
+// on affiche la premiere question
+// on 
+start.addEventListener("click", () => {
+    question1.style.display = "block"
+    start.setAttribute("disabled", "true");
+    start.style.opacity = ".5"
+})
+
+// apres la reponse de user on passe la question suivante :
+const btns = document.querySelectorAll(".question1")
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (btn.id === "good-answer") {
+            score++
+            question1.style.display = "none"
+            question2.style.display = "block"
+        } else {
+            question1.style.display = "none"
+            question2.style.display = "block"
+        }
+
+    })
+})
