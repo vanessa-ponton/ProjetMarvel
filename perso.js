0af1ec8a5d34699a389a83fdb7cf65f8a0587301
Nadir KHOUANI <khouani.nadir7@gmail.com>
2023-11-29 11:46:00 +0100

    persojs

---
+++ perso.js
@@ -0,0 +1,27 @@
+const cards = [
+    {
+        src: "Images/kang-le-conquerant.jpg",
+        title: "Kang Le Conquérant",
+        description: "Voyageant à travers le temps, Kang est un super-vilain cosmique. Doté d'une intelligence exceptionnelle, il manipule les réalités pour asseoir son pouvoir tyrannique,posant des défis incommensurables",
+    }
+]
+{/* <article class="card">
+<img class="card-img" src="Images/kang-le-conquerant.jpg" alt="kang-le-conquerant">
+<div class="card-content">
+    <h1 class="card-title">Kang Le Conquérant</h1>
+    <p class="card-p">Voyageant à travers le temps, Kang est un super-vilain cosmique. Doté d'une
+        intelligence exceptionnelle, il manipule les réalités pour asseoir son pouvoir tyrannique,
+        posant des défis incommensurables.
+    </p>
+</div>
+</article> */}
+
+// cibler la balise div avec la classe cards
+// creer la balise article avec la classe card 
+// injecter article dans la div cards 
+// on va creer un element pour chaque arguments de l'objet et le injecter dans l'article 
+// repeter les meme operations pour tout notre tableau 
+
+cards.map(() => {
+
+})
